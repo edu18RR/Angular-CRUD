166e7b68eaa3919ca4d12a155e3963cf34efdd38
edu18RR <edu.rosas.16@gmail.com>
2023-10-13 08:08:41 -0600

    actualizacion de estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';  // CRUD services API
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // CRUD services API
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCrud2';
+title(title: any) {
+  throw new Error('Method not implemented.');
+}
+employeeArray: Employee[] = [
+  {id: 1, name: "Eduardo", country: "Mexico"},
+  {id: 2, name: "Emma", country: "USA"},
+  {id: 3, name: "Veronica", country: "Canada"}
+  ];
 }
